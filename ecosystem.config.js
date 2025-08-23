035e29979b4551872ebd51b0ad0c0738960a1fc4
sujal <tanksujal4545@gmail.com>
2025-08-23 22:56:18 +0530

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,15 +13,5 @@ module.exports = {
         CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk"
       }
     },
-    {
-      name: "frontend",
-      script: "npx",
-      args: "serve -s build -l 3001", // serve build at port 3001
-      cwd: "./frontend",              // set working directory to frontend
-      env: {
-        REACT_APP_API_URL: "http://15.206.212.143:3000",
-        REACT_APP_CLOUDINARY_CLOUD_NAME: "dceaiwew4"
-      }
-    }
   ]
 };
