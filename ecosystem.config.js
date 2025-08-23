beb367954dfd5ef43e8e3217cd1878fb74703f52
sujal <tanksujal4545@gmail.com>
2025-08-23 22:44:50 +0530

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,25 @@ module.exports = {
   apps: [
     {
       name: "backend",
-      script: "server.js", // change to your entry file (e.g., index.js or app.js)
+      script: "server.js",     // entry file (inside backend folder)
+      cwd: "./backend",        // set working directory to backend
       watch: true,
       env: {
         PORT: 3000,
         SECRET_KEY_JWT: "sectetkeyofjwtiskainaiok",
         CLOUDINARY_CLOUD_NAME: "dceaiwew4",
         CLOUDINARY_API_KEY: "929939487612843",
-        CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk",
+        CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk"
+      }
+    },
+    {
+      name: "frontend",
+      script: "npx",
+      args: "serve -s build -l 3001", // serve build at port 3001
+      cwd: "./frontend",              // set working directory to frontend
+      env: {
+        REACT_APP_API_URL: "http://15.206.212.143:3000",
+        REACT_APP_CLOUDINARY_CLOUD_NAME: "dceaiwew4"
       }
     }
   ]
