2d43a4b523316b9e5a1978f609c455f62b1a6c6b
sujal <tanksujal4545@gmail.com>
2025-08-24 20:18:55 +0530

    changes of comments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,33 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "civico-backend",
+//       script: "server.js",
+//       cwd: "./backend",
+//       watch: true,
+//       env: {
+//         PORT: 3000,
+//         NODE_ENV: "production",
+//         SECRET_KEY_JWT: "sectetkeyofjwtiskainaiok",
+//         CLOUDINARY_CLOUD_NAME: "dceaiwew4",
+//         CLOUDINARY_API_KEY: "929939487612843",
+//         CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk",
+//       }
+//     },
+//     {
+//       name: "civico-frontend",
+//       script: "npm",
+//       args: "run preview",  // if Vite frontend
+//       cwd: "./frontend",
+//       env: {
+//         PORT: 5173, // or 4173 depending on vite preview
+//         NODE_ENV: "production",
+//       }
+//     }
+//   ]
+// };
+
+
 module.exports = {
   apps: [
     {
@@ -11,17 +41,7 @@ module.exports = {
         SECRET_KEY_JWT: "sectetkeyofjwtiskainaiok",
         CLOUDINARY_CLOUD_NAME: "dceaiwew4",
         CLOUDINARY_API_KEY: "929939487612843",
-        CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk",
-      }
-    },
-    {
-      name: "civico-frontend",
-      script: "npm",
-      args: "run preview",  // if Vite frontend
-      cwd: "./frontend",
-      env: {
-        PORT: 5173, // or 4173 depending on vite preview
-        NODE_ENV: "production",
+        CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk"
       }
     }
   ]
