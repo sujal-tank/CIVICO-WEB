bb6d13cd10f6720b2838794a979b103e4557143d
sujal <tanksujal4545@gmail.com>
2025-08-24 19:27:43 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       cwd: "./backend",
       watch: true,
       env: {
-        PORT: 4000,
+        PORT: 3000,
         NODE_ENV: "production",
         SECRET_KEY_JWT: "sectetkeyofjwtiskainaiok",
         CLOUDINARY_CLOUD_NAME: "dceaiwew4",
@@ -14,6 +14,15 @@ module.exports = {
         CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk",
       }
     },
-   
+    {
+      name: "civico-frontend",
+      script: "npm",
+      args: "run preview",  // if Vite frontend
+      cwd: "./frontend",
+      env: {
+        PORT: 5173, // or 4173 depending on vite preview
+        NODE_ENV: "production",
+      }
+    }
   ]
 };
