4495aeda5ae11c3d5a1a153b71d64e3c9711fb3a
sujal <tanksujal4545@gmail.com>
2025-08-24 16:58:02 +0530

    add env configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,8 @@ module.exports = {
         SECRET_KEY_JWT: "sectetkeyofjwtiskainaiok",
         CLOUDINARY_CLOUD_NAME: "dceaiwew4",
         CLOUDINARY_API_KEY: "929939487612843",
-        CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk"
+        CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk",
+        NODE_ENV :"PRODUCTION"
       }
     },
   ]
