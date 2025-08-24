ba75338811fc055f177c6e860652f2b9c2e89311
sujal <tanksujal4545@gmail.com>
2025-08-24 17:09:32 +0530

    chnage react url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "backend",
-      script: "server.js",     // entry file (inside backend folder)
-      cwd: "./backend",        // set working directory to backend
+      name: "civico-app",
+      script: "server.js",     // entry point
+      cwd: "./backend",        // backend folder
       watch: true,
       env: {
         PORT: 3000,
@@ -11,8 +11,9 @@ module.exports = {
         CLOUDINARY_CLOUD_NAME: "dceaiwew4",
         CLOUDINARY_API_KEY: "929939487612843",
         CLOUDINARY_API_SECRET: "ESUbW1n-e6_K0WeUPJ0ESoodNgk",
-        NODE_ENV :"PRODUCTION"
+        NODE_ENV: "PRODUCTION",
+        VITE_API_URL: "http://civico.co.in:3000/api"
       }
-    },
+    }
   ]
 };
